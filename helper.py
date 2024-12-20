b0571e9556461b1ffc48f94e67261795fc1366e9
Martina Pauer <martinapauer38@gmail.com>
2024-12-20 19:31:15 -0300

    Actualización con todo
    mejor interfaz GTK, Listado de archivos para hacer hojas de calculo en archivos CSV y optimizaciones).

--- helper.py
+++ helper.py
@@ -21,7 +21,7 @@ def luhn(numero:str) -> dict:
         Aplica algoritmo de Luhn\npatente US-2950048A.\n\nA partir de texto con numero, devuelve un diccionario\ncon todos los resultados de aplicar algoritmo de Luhn\n\n claves de accesos:\n\t[valido, digito_de_control, suma_de_verificacion, formula_de_verificacion].
     '''
     # Conversion por si pasan un numero, codigo malicioso u otra cosa distinta a cadena
-    numero = str(numero)
+    numero = numero.__str__()
     # Paso 1: Obtiene numero de control, primer digito del numero
     suma_de_verificacion = 0
     # Ciclo para aplicar luhn
@@ -39,10 +39,11 @@ def luhn(numero:str) -> dict:
     digito_de_control = int(numero[0])
     # Debe dar digito de control y devuelvo como una lista para enriquecer la informacion que brinda
     return {
-                'valido' : f'¿Numero de Luhn Valido? {(formula_de_verificacion == digito_de_control)}',
-                'control' : f'Digito de control (check digit): {digito_de_control}',
-                'verificacion' : f'Suma de Verificacion: {suma_de_verificacion}',
-                'formula' : f'Diferencia entre 10 y resto de dividir Suma de Verificacion y 10: {formula_de_verificacion}'
+                'numero' : f'{numero.__str__()},',
+                'valido' : f'{(formula_de_verificacion == digito_de_control).__str__()},',
+                'control' : f'{digito_de_control.__str__()},',
+                'verificacion' : f'{suma_de_verificacion.__str__()},',
+                'formula' : f'{formula_de_verificacion.__str__()}'
             }
 
 class Helper(Gtk.Window):
@@ -56,9 +57,9 @@ class Helper(Gtk.Window):
 
         # Defino widgets a usar
 
-        self.contenedor = Gtk.VBox(spacing = 190)
+        self.contenedor = Gtk.VBox(spacing = 150)
 
-        self.etiqueta_para_entrada = Gtk.Label(label = 'Validar número usando algoritmo de Luhn:')
+        self.etiqueta_para_entrada = Gtk.Label(label = '\nValidar número usando algoritmo de Luhn:')
 
         self.entrada = Gtk.Entry()
         # Conecto evento para caundo cambia texto en entrada
@@ -95,7 +96,7 @@ class Helper(Gtk.Window):
         # Voy iterando sobre cada salida
         for clave in texto.keys():
             # Sumo posible salida a resultado con algoritmo de Luhn
-            resultado += f'\t{texto[clave.__str__()].__str__()}\t\n\n'
+            resultado += f'\t{clave.__str__()} : {texto[clave.__str__()].__str__()}\t\n\n'
         # Elimino variable que no necesito por seguridad y rendimiento
         del texto
         # Actualizo salida
@@ -120,12 +121,68 @@ if ((sys.argv.__len__() == 1) and (__name__ == '__main__')):
         # Cierro ventana
         validador.close()
         # Libero memoria
-        del luhn, sumar_digitos, validador, Helper
-elif (sys.argv.__contains__('-n')) and (__name__ == '__main__'):
+        del validador, Helper
+
+elif ((sys.argv.__contains__('-f')) and (__name__ == '__main__')):
+    # Leo cada numero en archivo (primera columna hasta no encontrar mas columnas con numero)
+    archivo = open(sys.argv[sys.argv.index('-f') + 1], 'r')
+    # Archivo donde guardar las caracteristicas con los numeros que se van descubriendo (append para que se pueda ejecutar por partes)
+    salida_archivo = open(f'{archivo.name.__str__().replace(".", "_")}-luhn-pauer.csv', 'a')
+
+    for linea in archivo.readlines():
+        # Obtengo el numero
+        try:
+            # Obtengo numero a analizar
+            numero = int(linea)
+            # Obtengo caracteristicas de la salida del numero de luhn
+            caracteristicas = luhn(numero)
+            # Agrego lineas
+            salida_archivo.write('\n')
+            # Itero sobre cada una de las claves de acceso al diccionario de salida del algoritmo
+            for caracteristica in caracteristicas.keys():
+                # Escribo a archivo de salida cada caracteristica encontrada
+                try:
+                    salida_archivo.write(f'{caracteristicas[caracteristica].__str__()}')
+                except:
+                    # Corto ejecucion si no se puede escribir en archivo
+                    print(f'Error al escribir archivo "{salida_archivo.name.__str__()}" en ciclo secundario')
+                    break
+        # Puede haber error de lectura
+        except:
+            print(f'\nError al leer archivo "{archivo.name.__str__()}"\n')
+    # Fin de la escritura de archivo con datos de salida
+    print(f'\n\tRevise si archivo "{salida_archivo.name.__str__()}" fue escrito\t\n')
+    # Cierro archivos para que sea accesible por el sistema
+
+    archivo.close()
+
+    salida_archivo.close()
+
+elif ((sys.argv.__contains__('-n')) and (__name__ == '__main__')):
     # Obtengo salida de algoritmo de luhn
     texto = luhn(int(sys.argv[sys.argv.index('-n') + 1]))
     # Muestro resultado de cada clave de la salida
     for clave in texto.keys():
         print(f'\n\t{clave.__str__()} : {texto[clave].__str__()}\n\n')
+
+elif ((sys.argv.__contains__('-t')) and (__name__ == '__main__')):
+    # Programa clasico de entrada y salida por terminal con validacion de datos
+    print('\nVALIDA NÚMERO USANDO ALGORITMO DE LUHN\n')
+    # Validacion
+    while True:
+        try:
+            salida = luhn(int(input('\tIngrese número para validar: ')))
+            # Sigo hasta que falle
+            # Muestro resultado de cada clave de la salida
+            for dato in salida.keys():
+                print(f'\n\t{dato.__str__()} : {salida[dato].__str__()}\n')
+            # Corto bucle si todo sale bien
+            break
+        except:
+            # Continuo bucle si todo sale mal
+            print('\nINGRESE NUMERO\n')
+            continue
 else:
-    print('\nOpciones:\n\n\t-n <numero-a-valiadar>\n\n\t-f <archivo-con-lista-de-numeros>\n\n\t-t (shell interactiva)\n\n')
+    print('\nOpciones:\n\n\tNINGUNA (sin argumentos) - Ejecuta programa gráfico con toolkit PyGtk mediante GObject\n\n\t-n <numero-a-valiadar>\n\n\t-f <archivo-con-lista-de-numeros>\n\n\t-t (shell interactiva)\n\n')
+# Elimino datos que ya no necesito fuera del condicional paa que siempre se haga
+del luhn, sumar_digitos
